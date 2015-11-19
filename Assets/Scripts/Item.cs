5e4fb3c9d7766800923c128f1edeb567f9c29b2c
Ainhoa LM <ainhoalopezmarful@gmail.com>
2015-11-19 11:52:16 +0100

    14. Generando puntos

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Item : MonoBehaviour
+{
+    public int puntosGanados = 5;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // OnTriggerEnter2D is called when the Collider2D other enters the trigger (2D physics only)
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        NotificationCenter.DefaultCenter().PostNotification(this, "IncrementarPuntos", puntosGanados);
+        Destroy(gameObject);
+        //Debug.Log("Tocado");
+    }
+}
