48ed06b281b2c7d8852900bb69bae3fb026ae426
Ainhoa LM <ainhoalopezmarful@gmail.com>
2015-11-19 10:49:55 +0100

    13. Ganando puntos

---
+++ Assets/Scripts/Puntuacion.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Puntuacion : MonoBehaviour {
+
+    private int puntuacion = 0;
+
+	// Use this for initialization
+	void Start () {
+        //Nos suscribimos a la notificación de ganar puntos
+        NotificationCenter.DefaultCenter().AddObserver(this,"IncrementarPuntos");
+	}
+	
+    void IncrementarPuntos(Notification notifacacion){
+        int puntosAIncrementar = (int)notifacacion.data;
+        puntuacion += puntosAIncrementar;
+        Debug.Log("Incrementando: "+ puntosAIncrementar+"puntos. Total ganados: "+puntuacion);
+    }
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
